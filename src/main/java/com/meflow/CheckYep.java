79c5268d39ba15ea4702a706b156f88c42115667
Hossam Elabd <halabd@alriyadh.gov.sa>
2021-03-18 12:00:06 +0300

    zero Point

---
+++ src/main/java/com/meflow/CheckYep.java
@@ -0,0 +1,16 @@
+package com.meflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CheckYep {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckYep.class, args);
+
+	}
+
+}
+
